745ce3bd507642f53fdf5d17bf326433d1ba7ad3
Benjamin Delgado <zomvroficial@gmail.com>
2026-06-16 00:16:05 -0400

    feat: Primera parte de la implementación de GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import gui.MainWindow;
+
 // Benjamín Felipe Delgado Sánchez - 22.223.703-3
 public class Main {
     public static void main() {
-        System.out.println("HOla Mundo!");
+        MainWindow mainWindow = new MainWindow();
     }
 }
---
+++ src/gui/MainWindow.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("TCG Manager");
+        setSize(1000, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
